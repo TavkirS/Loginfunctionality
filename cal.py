e4ccf8601ff3eda2dc46ab68fb92c21993eb8d76
TavkirS <taukirofficial15@gmail.com>
2025-08-21 21:09:43 +0530

    Today i added the multi and div functionilty

--- cal.py
+++ cal.py
@@ -1,8 +1,10 @@
-print("Simple Calculator - Version 1")
+print("Simple Calculator - Version 2")
 print("1. Add")
 print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
 
-choice = input("Enter choice (1/2): ")
+choice = input("Enter choice (1/2/3/4): ")
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
@@ -14,6 +16,17 @@ if choice == "1":
 elif choice == "2":
     result = num1 - num2
     print("Result:", result)
+
+elif choice == "3":
+    result = num1 * num2
+    print("Result:", result)
+
+elif choice == "4":
+    if num2 == 0:
+        print("Error! Division by zero.")
+    else:
+        result = num1 / num2
+        print("Result:", result)
 
 else:
     print("Invalid choice")
